334a59d1aa53c67bac7e1eead0de1ce94e631fc5
Anthotho <anthony.jambon1985@gmail.com>
2023-01-25 16:55:43 +0100

    modif settings.py

--- stock/stock/settings.py
+++ stock/stock/settings.py
@@ -127,8 +127,8 @@ USE_L10N = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_URL = '/static/'
+MEDIA_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
